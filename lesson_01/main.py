ebe5e629beca49974078439e5130bb14c516077c
vbelous <vbbelous@gmail.com>
2018-10-10 12:22:39 +0300

    1

--- lesson_01/main.py
+++ lesson_01/main.py
@@ -4,11 +4,16 @@ from lesson_01.stack_exceptions import *
 class Stack:
 
     def __init__(self, data_type=object(), limit=None):
+        """"Конструктор принимает опциональный аргумент data_type, по умолчанию - object,
+        опциональный аргумент limit (int), который определяет максимальный размер стэка, по умолчанию - None."""
         self.data_type = type(data_type)
         self.limit = limit if limit is not None else -1
         self.my_stack = list()
 
     def _push(self, new_element):
+        """"проверяет возможность добавления элемента в стэк (по лимиту и типу).
+           В случае несоответствия типов должен генерить исключение TypeError.
+           В случае достижения лимита - генерить LimitExceedError."""
         if len(self.my_stack) == 0 and self.data_type == type(object()):
             self.data_type = type(new_element)
 
@@ -18,10 +23,13 @@ class Stack:
             raise LimitExceedError
 
     def push(self, new_element):
+        """добавляет новый объект в стэк"""
         self._push(new_element)
         self.my_stack.append(new_element)
 
     def pull(self):
+        """"извлекает верхний элемент стэка и возвращает его.
+        В случае пустого стэка генерит исключение EmptyStackError"""
         if len(self.my_stack) > 0:
             result = self.my_stack[-1]
             self.my_stack.remove(result)
@@ -30,16 +38,20 @@ class Stack:
             raise EmptyStackError
 
     def count(self):
+        """возвращает количество элементов в стэке"""
         return len(self.my_stack)
 
     def clear(self):
+        """очищает стэк"""
         self.my_stack.clear()
 
     @property
     def type(self):
+        """возвращает тип данных стэка."""
         return self.data_type.__name__
 
     def __str__(self):
+        """Возвращающет строку вида Stack<тип данных>"""
         return "\"{}<{}>\"".format(str(type(self).__name__), str(self.type))
 
 
