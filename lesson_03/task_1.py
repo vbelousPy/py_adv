36e4ca4808f2000a7a824fa9dfbc65f423a334d8
vbelous <vbbelous@gmail.com>
2018-10-23 13:30:01 +0300

    lesson_03.1

---
+++ lesson_03/task_1.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+
+def revers_file(file_path):
+    with open(file_path, "rb+") as f:
+        f_len = len(f.read())
+        for i in range(f_len):
+            for j in range(f_len - i - 1):
+                f.seek(j)
+                current_symbols = f.read(2)
+                f.seek(j)
+                f.write(current_symbols[::-1])
+                j += 1
+
+
+file_name = sys.argv[1]
+if os.path.exists(file_name):
+    revers_file(file_name)
+else:
+    print("file not found")
