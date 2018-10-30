25abbfdb82a9399ad8e5665108f2f40adad8a7e8
vbelous <vbbelous@gmail.com>
2018-10-30 13:04:41 +0200

    lesson_04.1

--- lesson_04/task_1.py
+++ lesson_04/task_1.py
@@ -6,13 +6,16 @@ class Context:
 
     def __init__(self, **kwargs) -> None:
         for k, v in kwargs.items():
-            self.context_dict.update({k: v})
+            if match("[a-zA-Z]+\\w*", k):
+                self.__setattr__(k, v)
+            else:
+                raise NameError
 
     def __setattr__(self, name, value):
-        self.context_dict.update({name: value})
+        self.__dict__[name] = value
 
     def __getattr__(self, name):
-        some_value = self.context_dict.get(name)
+        some_value = self.__dict__.get(name)
         if some_value is None:
             raise NameError
         else:
@@ -20,28 +23,20 @@ class Context:
 
     def __str__(self):
         result = str()
-        for k, v in self.context_dict.items():
+        for k, v in self.__dict__.items():
             result += k + "=" + str(v) + ", "
         return self.__class__.__name__ + " (" + result[:-2] + ")"
 
     def __len__(self):
-        dict_len = 0
-        for k in self.context_dict.keys():
-            if match("[a-zA-Z]+\\w*", k):
-                dict_len += 1
-            else:
-                raise NameError
-        return dict_len
+        return len(self.__dict__)
 
     def __iter__(self):
-        return iter(self.context_dict.items())
+        return iter(self.__dict__.items())
 
 
-my_context = Context(**{"1k": 12, "_3s": 9, "s2s": 0})
+my_context = Context(**{"z1": 12, "z2": 9, "z3": 0})
 my_context.x1 = 1
 my_context.t14 = 23
 my_context.w = 7
 print(my_context)
-# print("size =", len(my_context))
-for key, val in my_context:
-    print(key, "=", val)
+print("size =", len(my_context))
